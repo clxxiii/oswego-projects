445a76a6901d61e7313940464da9c751f1b0331d
clxxiii <earthnoartisjusteh@gmail.com>
2021-09-15 11:38:57 -0400

    Finished CaughtSpeeding challenge

--- Extras/src/CaughtSpeeding.java
+++ Extras/src/CaughtSpeeding.java
@@ -1,7 +1,13 @@
 public class CaughtSpeeding {
     public static void main(String[] args) {
-        int speed = 65;
-        boolean isBirthday = false;
-        
+        System.out.println("(61, false) Ticket Value: " + caughtSpeeding(61, false));
+        System.out.println("(65, true) Ticket Value: " + caughtSpeeding(65, true));
+    }
+
+    private static int caughtSpeeding(int speed, boolean isBirthday) {
+        if (isBirthday) { speed -= 5; }
+        if (speed >= 81) { return 2; }
+        if (speed >= 61) { return 1; }
+        return 0;
     }
 }
