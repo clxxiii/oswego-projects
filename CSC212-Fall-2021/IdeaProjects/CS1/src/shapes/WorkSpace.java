2e14a9b66db360bef0c2431b8a20d51ea7ecea0d
efereira <efereira@oswego.edu>
2021-10-01 15:35:47 -0400

    trying to fix the compiled files

--- CSC212-Fall-2021/IdeaProjects/CS1/src/shapes/WorkSpace.java
+++ CSC212-Fall-2021/IdeaProjects/CS1/src/shapes/WorkSpace.java
@@ -4,6 +4,13 @@ public class WorkSpace {
 
     public static void main(String[] args) {
         SRectangle desk = new SRectangle(66,152);
+        SRectangle notebook = new SRectangle(21, 29.7);
+        SRectangle labManual = new SRectangle(21, 29.7);
+        SCircle coaster = new SCircle(3.175);
+        SCircle plates = new SCircle(20);
+
+//        double coveredDesk =
+//                ( notebook. * 2) +
 
     }
 }
