0430cece9cee35e0b779916268406880a096b0b3
clxxiii <earthnoartisjusteh@gmail.com>
2022-02-02 15:42:23 -0500

    Assignment 1 progress

--- CSC241-Spring-2022/assignment01/GradeManager.java
+++ CSC241-Spring-2022/assignment01/GradeManager.java
@@ -1,5 +1,9 @@
 package assignment01;
 
+import java.io.File;
+import java.nio.file.*;
+import java.util.Scanner;
+
 /*
     CSC 241 Spring 2022
     Assignment 1
@@ -8,7 +12,31 @@ package assignment01;
  */
 
 public class GradeManager {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        boolean noExit = true;
+        // String studentName = promptForInput("Enter a students name: ");
 
+        Path p = Paths.get(System.getProperty("user.dir"), "assignment01", "John.txt");
+
+        System.out.println(p.toString());
+
+        // File studentFile = new File(p.toString());
+
+        // Scanner sc = new Scanner(studentFile);
+
+        // System.out.println(sc.nextLine());
+
+        // while (noExit) {
+
+        // noExit = false;
+        // }
+    }
+
+    private static String promptForInput(String msg) {
+        System.out.print(msg);
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        sc.close();
+        return name;
     }
 }
