099827cc6042afde7ff789dbd4311a9a9b11986d
clxxiii <earthnoartisjusteh@gmail.com>
2022-02-02 18:36:24 -0500

    assignment01 done!

--- CSC241-Spring-2022/src/assignment01/GradeManager.java
+++ CSC241-Spring-2022/src/assignment01/GradeManager.java
@@ -1,5 +1,6 @@
 package assignment01;
 
+import java.io.*;
 import java.nio.file.*;
 import java.util.Scanner;
 
@@ -11,31 +12,93 @@ import java.util.Scanner;
  */
 
 public class GradeManager {
-    public static void main(String[] args) throws Exception {
-        boolean noExit = true;
-        // String studentName = promptForInput("Enter a students name: ");
+	public static void main(String[] args) throws FileNotFoundException {
 
-        Path p = Paths.get(System.getProperty("user.dir"), "assignment01", "John.txt");
+		// Prompt for file name
+		String studentName = promptForInput("Enter a students name: ", false);
+		Path studentPath = Paths.get(System.getProperty("user.dir"), "src", "assignment01", studentName + ".txt");
+		File studentFile = new File(studentPath.toString());
 
-        System.out.println(p.toString());
+		// Read selected file
+		Scanner sc = new Scanner(studentFile);
+		String[] info = new String(sc.nextLine()).split(",");
+		sc.close();
+		printInfoString(info);
 
-        // File studentFile = new File(p.toString());
+		String editOrQuit = promptForInput("Do you want to edit or quit? (edit | quit)? ", false);
 
-        // Scanner sc = new Scanner(studentFile);
+		if (editOrQuit.equalsIgnoreCase("edit")) {
+			try {
+				String input = promptForInput("Enter a course work you want to change (q1,q2,q3,mid,final): ", false);
 
-        // System.out.println(sc.nextLine());
+				// Replace input with array index
+				int index;
+				if (input.equalsIgnoreCase("Q1"))
+					index = 2;
+				else if (input.equalsIgnoreCase("Q2"))
+					index = 3;
+				else if (input.equalsIgnoreCase("Q3"))
+					index = 4;
+				else if (input.equalsIgnoreCase("mid"))
+					index = 5;
+				else if (input.equalsIgnoreCase("final"))
+					index = 6;
+				else
+					throw new Throwable("User input is outside of the supported values.");
+				String newScore = promptForInput("\nEnter a new score: ", true);
+				info[index] = newScore;
 
-        // while (noExit) {
+				String dataString = String.join(",", info);
 
-        // noExit = false;
-        // }
-    }
+				writeDataToFile(studentFile, studentPath, dataString);
 
-    private static String promptForInput(String msg) {
-        System.out.print(msg);
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        sc.close();
-        return name;
-    }
+			} catch (Throwable e) {
+				System.out.println(e.toString());
+			}
+		}
+
+	}
+
+	private static void writeDataToFile(File studentFile, Path studentPath, String dataString)
+			throws IOException {
+		File outputFile = new File(studentPath.toString() + ".temp");
+		FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+		BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+		bufferedWriter.write(dataString, 0, dataString.length());
+
+		bufferedWriter.close();
+
+		studentFile.delete();
+		outputFile.renameTo(new File(studentPath.toString()));
+	}
+
+	/**
+	 * Format info in readable format:
+	 * Name: John | ID: 123456789 | Q1: 10 | Q2: 8 | Q3: 7 | Midterm: 87 | Final: 91
+	 */
+	private static void printInfoString(String[] info) {
+		System.out.println(
+				"Name: " + info[0] + " | " +
+						"ID: " + info[1] + " | " +
+						"Q1: " + info[2] + " | " +
+						"Q2: " + info[3] + " | " +
+						"Q3: " + info[4] + " | " +
+						"Midterm: " + info[5] + " | " +
+						"Final: " + info[6]);
+	}
+
+	/**
+	 * Sends a message to the console and returns the user input
+	 */
+	private static String promptForInput(String msg, boolean closeScanner) {
+		System.out.print(msg);
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		if (closeScanner)
+			sc.close();
+		return input;
+	}
+
 }
