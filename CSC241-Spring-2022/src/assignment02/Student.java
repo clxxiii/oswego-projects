4c35be3d14a3606e81b1189b79cc8eb2c77f6b9c
clxxiii <earthnoartisjusteh@gmail.com>
2022-02-14 13:21:27 -0500

    Assignment 2 template files

---
+++ CSC241-Spring-2022/src/assignment02/Student.java
@@ -0,0 +1,22 @@
+package assignment02;
+
+/*
+    CSC 241 Spring 2022
+    Assignment 1
+    Name: Eli Fereira
+    ID: 806061464
+ */
+
+class Student {
+    String name;                // student's name
+    private String id;          // student's id
+
+    private int[] quiz;         // quiz scores (5 quizzes)
+    private int mid;            // midterm score
+    private int finalExam;      // final score
+
+    // more variables may be added
+
+    // complete the class as described in assignment2.pdf
+    // Keys: private variables should be accessed through access methods
+}
