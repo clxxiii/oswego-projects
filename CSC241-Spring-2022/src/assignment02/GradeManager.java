fb34f5723b40be4b5f718d831206281a9c96f7b4
clxxiii <earthnoartisjusteh@gmail.com>
2022-02-23 22:35:09 -0500

    add space for input

--- CSC241-Spring-2022/src/assignment02/GradeManager.java
+++ CSC241-Spring-2022/src/assignment02/GradeManager.java
@@ -8,43 +8,41 @@ public class GradeManager {
 		System.out.print("Enter a course code: ");
 		Scanner sc = new Scanner(System.in);
 		Section section = new Section(sc.nextLine());
+		while (true) {
+			System.out.print("Select menu [edit | quit]? ");
+			if (!(sc.nextLine().equalsIgnoreCase("edit"))) {
+				sc.close();
+				return;
+			}
 
-		System.out.print("Select menu [edit | quit]? ");
-		String command = sc.nextLine();
-		if (!(command.equalsIgnoreCase("edit"))) {
-			sc.close();
-			return;
+			System.out.print("Enter a student: ");
+			Student selectedStudent = section.getStudent(sc.nextLine());
+
+			System.out.print("Enter a coursework you want to edit (q1,q2,q3,mid,final): ");
+
+			String input = sc.nextLine();
+			// Replace input with array index
+			int index;
+			if (input.equalsIgnoreCase("Q1"))
+				index = 2;
+			else if (input.equalsIgnoreCase("Q2"))
+				index = 3;
+			else if (input.equalsIgnoreCase("Q3"))
+				index = 4;
+			else if (input.equalsIgnoreCase("mid"))
+				index = 5;
+			else if (input.equalsIgnoreCase("final"))
+				index = 6;
+			else {
+				System.out.println("User input is outside of the supported values.");
+				sc.close();
+				return;
+			}
+
+			System.out.print("Enter a new score: ");
+			int gradeChange = Integer.valueOf(sc.nextLine());
+
+			selectedStudent.changeInfo(index, gradeChange);
 		}
-
-		System.out.print("Enter a student: ");
-		Student selectedStudent = section.getStudent(sc.nextLine());
-
-		System.out.print("Enter a coursework you want to edit (q1,q2,q3,mid,final):");
-
-		String input = sc.nextLine();
-		// Replace input with array index
-		int index;
-		if (input.equalsIgnoreCase("Q1"))
-			index = 2;
-		else if (input.equalsIgnoreCase("Q2"))
-			index = 3;
-		else if (input.equalsIgnoreCase("Q3"))
-			index = 4;
-		else if (input.equalsIgnoreCase("mid"))
-			index = 5;
-		else if (input.equalsIgnoreCase("final"))
-			index = 6;
-		else {
-			System.out.println("User input is outside of the supported values.");
-			sc.close();
-			return;
-		}
-
-		System.out.print("Enter a new score: ");
-		int gradeChange = sc.nextInt();
-
-		selectedStudent.changeInfo(index, gradeChange);
-
-		sc.close();
 	}
 }
