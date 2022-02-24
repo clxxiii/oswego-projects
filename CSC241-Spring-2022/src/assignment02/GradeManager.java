d6122fff4cc04a52b05163e6badb93e896b7f6d7
clxxiii <earthnoartisjusteh@gmail.com>
2022-02-23 22:18:20 -0500

    LETS GO BABY

--- CSC241-Spring-2022/src/assignment02/GradeManager.java
+++ CSC241-Spring-2022/src/assignment02/GradeManager.java
@@ -1,23 +1,50 @@
 package assignment02;
 
-import java.nio.file.*;
 import java.util.Scanner;
 
-/*
-    CSC 241 Spring 2022
-    Assignment 1
-    Name: Eli Fereira
-    ID: 806061464
- */
-
 public class GradeManager {
-    public static void main(String[] args) throws Exception {
-        // Get course code and print
-        System.out.print("Enter a course code: ");
-        Scanner sc = new Scanner(System.in);
-		Path studentPath = Paths.get(System.getProperty("user.dir"), "src", "assignment02","data", sc.next() + ".txt");
+	public static void main(String[] args) throws Throwable {
+		// Get course code and print
+		System.out.print("Enter a course code: ");
+		Scanner sc = new Scanner(System.in);
+		Section section = new Section(sc.nextLine());
+
+		System.out.print("Select menu [edit | quit]? ");
+		String command = sc.nextLine();
+		if (!(command.equalsIgnoreCase("edit"))) {
+			sc.close();
+			return;
+		}
+
+		System.out.print("Enter a student: ");
+		Student selectedStudent = section.getStudent(sc.nextLine());
+
+		System.out.print("Enter a coursework you want to edit (q1,q2,q3,mid,final):");
+
+		String input = sc.nextLine();
+		// Replace input with array index
+		int index;
+		if (input.equalsIgnoreCase("Q1"))
+			index = 2;
+		else if (input.equalsIgnoreCase("Q2"))
+			index = 3;
+		else if (input.equalsIgnoreCase("Q3"))
+			index = 4;
+		else if (input.equalsIgnoreCase("mid"))
+			index = 5;
+		else if (input.equalsIgnoreCase("final"))
+			index = 6;
+		else {
+			System.out.println("User input is outside of the supported values.");
+			sc.close();
+			return;
+		}
+
+		System.out.print("Enter a new score: ");
+		int gradeChange = sc.nextInt();
+
+		selectedStudent.changeInfo(index, gradeChange);
+
 		sc.close();
-		Section section = new Section(studentPath.toString());
-		System.out.println(section.name);
-    }
+	}
 }
