a37c14c281fd7eb873e41db882671c71c33a98ee
clxxiii <earthnoartisjusteh@gmail.com>
2022-03-31 18:31:32 -0400

    search array of students rather than JsonArray

--- CSC241-Spring-2022/src/assignment04/LinearSearch.java
+++ CSC241-Spring-2022/src/assignment04/LinearSearch.java
@@ -3,7 +3,7 @@ package assignment04;
 import javax.json.*;
 
 public class LinearSearch {
-	public static JsonArray sort(JsonArray startingArray) {
+	public static JsonArray search(Student[] array, String key, String searchString) {
 
 		return null;
 	}
