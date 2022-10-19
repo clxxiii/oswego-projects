eae3e9628d43b8fab97df66804abba8d4b0f55b2
clxxiii <earthnoartisjusteh@gmail.com>
2022-10-19 12:05:36 -0400

    assembly
    :

---
+++ 322/lectures/hello.s
@@ -0,0 +1,220 @@
+	.file	"hello.c"
+	.text
+.Ltext0:
+	.file 0 "/home/efereira/oswego-projects/322/lectures" "hello.c"
+	.section	.rodata
+.LC0:
+	.string	"Hello, world!"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.file 1 "hello.c"
+	.loc 1 4 1
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	.loc 1 5 5
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	call	puts@PLT
+	.loc 1 6 1
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+.Letext0:
+	.section	.debug_info,"",@progbits
+.Ldebug_info0:
+	.long	0x84
+	.value	0x5
+	.byte	0x1
+	.byte	0x8
+	.long	.Ldebug_abbrev0
+	.uleb128 0x2
+	.long	.LASF10
+	.byte	0x1d
+	.long	.LASF0
+	.long	.LASF1
+	.quad	.Ltext0
+	.quad	.Letext0-.Ltext0
+	.long	.Ldebug_line0
+	.uleb128 0x1
+	.byte	0x8
+	.byte	0x7
+	.long	.LASF2
+	.uleb128 0x1
+	.byte	0x4
+	.byte	0x7
+	.long	.LASF3
+	.uleb128 0x1
+	.byte	0x1
+	.byte	0x8
+	.long	.LASF4
+	.uleb128 0x1
+	.byte	0x2
+	.byte	0x7
+	.long	.LASF5
+	.uleb128 0x1
+	.byte	0x1
+	.byte	0x6
+	.long	.LASF6
+	.uleb128 0x1
+	.byte	0x2
+	.byte	0x5
+	.long	.LASF7
+	.uleb128 0x3
+	.byte	0x4
+	.byte	0x5
+	.string	"int"
+	.uleb128 0x1
+	.byte	0x8
+	.byte	0x5
+	.long	.LASF8
+	.uleb128 0x1
+	.byte	0x1
+	.byte	0x6
+	.long	.LASF9
+	.uleb128 0x4
+	.long	.LASF11
+	.byte	0x1
+	.byte	0x3
+	.byte	0x6
+	.quad	.LFB0
+	.quad	.LFE0-.LFB0
+	.uleb128 0x1
+	.byte	0x9c
+	.byte	0
+	.section	.debug_abbrev,"",@progbits
+.Ldebug_abbrev0:
+	.uleb128 0x1
+	.uleb128 0x24
+	.byte	0
+	.uleb128 0xb
+	.uleb128 0xb
+	.uleb128 0x3e
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0xe
+	.byte	0
+	.byte	0
+	.uleb128 0x2
+	.uleb128 0x11
+	.byte	0x1
+	.uleb128 0x25
+	.uleb128 0xe
+	.uleb128 0x13
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0x1f
+	.uleb128 0x1b
+	.uleb128 0x1f
+	.uleb128 0x11
+	.uleb128 0x1
+	.uleb128 0x12
+	.uleb128 0x7
+	.uleb128 0x10
+	.uleb128 0x17
+	.byte	0
+	.byte	0
+	.uleb128 0x3
+	.uleb128 0x24
+	.byte	0
+	.uleb128 0xb
+	.uleb128 0xb
+	.uleb128 0x3e
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0x8
+	.byte	0
+	.byte	0
+	.uleb128 0x4
+	.uleb128 0x2e
+	.byte	0
+	.uleb128 0x3f
+	.uleb128 0x19
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0xb
+	.uleb128 0x39
+	.uleb128 0xb
+	.uleb128 0x11
+	.uleb128 0x1
+	.uleb128 0x12
+	.uleb128 0x7
+	.uleb128 0x40
+	.uleb128 0x18
+	.uleb128 0x7c
+	.uleb128 0x19
+	.byte	0
+	.byte	0
+	.byte	0
+	.section	.debug_aranges,"",@progbits
+	.long	0x2c
+	.value	0x2
+	.long	.Ldebug_info0
+	.byte	0x8
+	.byte	0
+	.value	0
+	.value	0
+	.quad	.Ltext0
+	.quad	.Letext0-.Ltext0
+	.quad	0
+	.quad	0
+	.section	.debug_line,"",@progbits
+.Ldebug_line0:
+	.section	.debug_str,"MS",@progbits,1
+.LASF3:
+	.string	"unsigned int"
+.LASF2:
+	.string	"long unsigned int"
+.LASF6:
+	.string	"signed char"
+.LASF5:
+	.string	"short unsigned int"
+.LASF7:
+	.string	"short int"
+.LASF4:
+	.string	"unsigned char"
+.LASF8:
+	.string	"long int"
+.LASF11:
+	.string	"main"
+.LASF9:
+	.string	"char"
+.LASF10:
+	.string	"GNU C17 11.2.0 -mtune=generic -march=x86-64 -g -fasynchronous-unwind-tables -fstack-protector-strong -fstack-clash-protection -fcf-protection"
+	.section	.debug_line_str,"MS",@progbits,1
+.LASF0:
+	.string	"hello.c"
+.LASF1:
+	.string	"/home/efereira/oswego-projects/322/lectures"
+	.ident	"GCC: (Ubuntu 11.2.0-19ubuntu1) 11.2.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
