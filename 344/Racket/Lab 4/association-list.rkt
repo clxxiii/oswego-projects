6a7d1fdef6b21f31ce3af4cf1e83c3c7de59e5d4
clxxiii <earthnoartisjusteh@gmail.com>
2022-10-24 13:38:17 -0400

    start of Lab 4

---
+++ 344/Racket/Lab 4/association-list.rkt
@@ -0,0 +1,45 @@
+#lang racket
+
+( define ( generate-uniform-list l form )
+   ( cond
+      ( ( = l 0) '() )
+      ( else
+        ( cons form ( generate-uniform-list ( - l 1 ) form ) )
+        )
+      )
+   )
+
+( define ( a-list l1 l2 )
+   ( cond
+      ( ( not ( = ( length l1 ) ( length l2) ) ) '() )
+      ( ( = ( length l1) 0 ) '() )
+      ( else
+        ( cons
+          ( cons ( car l1 ) ( car l2 ) )
+          ( a-list ( cdr l1 ) (cdr l2 ) )
+          )
+        )
+      )
+   )
+
+
+( define ( assoc el li )
+   ( cond
+      ( ( = ( length li ) 0 ) '() )
+      ( ( equal? el ( car ( car li ) ) ) ( car li ) )
+      ( else
+        ( assoc el ( cdr li ) )
+        )
+      )
+   )
+
+( define ( rassoc el li )
+   ( cond
+      ( ( = ( length li ) 0 ) '() )
+      ( ( equal? el ( cdr ( car li ) ) ) ( car li ) )
+      ( else
+        ( rassoc el ( cdr li ) )
+        )
+      )
+   )
+
