56116d7cd0ed740b3ef51df7c0fe01053608992a
clxxiii <earthnoartisjusteh@gmail.com>
2022-11-09 15:07:53 -0500

    commit fun things

---
+++ 322/play/intInt.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	long long man = 3;
+	printf("Long Long; %lld\n", man);
+	printf("Size of Long Long %ld\n", sizeof(man));
+	printf("Size of Long: %ld\n", sizeof(long));
+	return 0;
+}
