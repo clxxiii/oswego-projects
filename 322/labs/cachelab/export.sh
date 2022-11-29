afd41c2c31ea6897e5b49ffde28d8fd287d88539
clxxiii <clxxiii@users.noreply.github.com>
2022-11-29 14:52:22 -0500

    Fix export function

--- 322/labs/cachelab/export.sh
+++ 322/labs/cachelab/export.sh
@@ -1,7 +1,8 @@
 rm export.tar.gz
 mkdir export
-cp cachelab.c ./export
-cp cachelab.h ./export
-cp cachelab_readme.txt ./export
-tar -czvf export.tar.gz ./export
+cp cachelab.c export
+cp cachelab.h export
+cp cachelab_readme.txt export
+cp cachelab export
+tar -czvf export.tar.gz export
 rm -rf export
