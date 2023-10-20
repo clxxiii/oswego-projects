97b4b5bd6aa3a1f140cc3d82fd558b005e624548
clxxiii <clxxiii@users.noreply.github.com>
2023-10-20 16:13:32 -0400

    init microbenchmark project

---
+++ 375/microbenchmark/src/main/java/dev/clxxiii/microbenchmark/MicrobenchmarkApplication.java
@@ -0,0 +1,13 @@
+package dev.clxxiii.microbenchmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrobenchmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrobenchmarkApplication.class, args);
+	}
+
+}
---
+++ 375/microbenchmark/src/test/java/dev/clxxiii/microbenchmark/MicrobenchmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.clxxiii.microbenchmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrobenchmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
